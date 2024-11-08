4099a27eb5564c189a4079b65b6c72d82bbe9a73
Jaume Fernández <jaumefernandez@carnicassunyer.com>
2024-11-08 19:25:10 +0100

    modelo comentario

--- models/Comment.js
+++ models/Comment.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema(
-  {
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    trip: { type: Schema.Types.ObjectId, ref: 'Trip', required: true },
-    content: { type: String, required: true },
-    parentComment: { type: Schema.Types.ObjectId, ref: 'Comment' },
-  },
-  { timestamps: true }
+    {
+        user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        trip: { type: Schema.Types.ObjectId, ref: 'Trip', required: true },
+        content: { type: String, required: true },
+        parentComment: { type: Schema.Types.ObjectId, ref: 'Comment' },
+    },
+    { timestamps: true }
 );
 
 module.exports = mongoose.model('Comment', commentSchema);
